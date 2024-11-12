81d858e4cfd96282b370d4e52ff8af96e6fb4178
Marcello Tridenti <marcello.tridenti@tutanota.com>
2024-11-12 13:58:50 +0100

    ﻿Updated config

--- docfx/build-pdf.bat
+++ docfx/build-pdf.bat
@@ -4,7 +4,3 @@ del pdf\docfx_articles\*.pdf
 echo Building VR Builder manual...
 docfx pdf docfx.json
 ren pdf\docfx_articles\core.pdf VR-Builder-Manual.pdf
-
-echo Building VR Builder Pro manual...
-docfx pdf docfx-pro.json
-ren pdf\docfx_articles\pro.pdf VR-Builder-Pro-Manual.pdf
